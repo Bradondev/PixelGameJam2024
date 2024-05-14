108bbc9cdb1c938f218fca2728d1a0aee1f995d0
Brandon Martin Del Campo <brandondevprog@gmail.com>
2024-05-14 15:31:36 -0700

    take player is able to take damage

--- GD Scripts/Player.gd
+++ GD Scripts/Player.gd
@@ -2,17 +2,19 @@ extends CharacterBody2D
 class_name  Player
 
 
-@export var MaxHealth :int
-
+@export var MaxHealth :int = 100
+@export var CurrentHealth: int = 100
 @export var inventory: Control
 
 @export var speed :int = 10000 # speed in pixels/sec
 @export var PlayerInventory:InventoryView
 @export var ui: CanvasLayer 
 @onready var gun: Node2D = $Gun
+@onready var mid_bar: TextureProgressBar = $UI/BotMargin/Hbox/Player/Resources/MidBar
 
 
 func  _ready() -> void: 
+	UpdataProgress()
 	pass
 func _physics_process(delta):
 	var direction = Input.get_vector("Left", "Right", "Up", "Down")
@@ -31,3 +33,13 @@ func FlipInventory():
 
 func UpdateSpirt():
 	pass
+
+func TakeDamage(Amount:int):
+	CurrentHealth -= Amount
+	if CurrentHealth <=0:
+		CurrentHealth = 0
+	UpdataProgress()
+	
+func UpdataProgress():
+	mid_bar.value = CurrentHealth
+	mid_bar.max_value = MaxHealth
--- Scene_Sub/BulletPackScene/BulletScene.gd
+++ Scene_Sub/BulletPackScene/BulletScene.gd
@@ -24,3 +24,8 @@ func _on_timer_timeout() -> void:
 
 
 
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("TakeDamage"):
+		body.TakeDamage(2)
