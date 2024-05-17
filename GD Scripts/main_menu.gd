ec2907fcd515dd1dff87d6ede573fe5dfa8d492f
DC125Konscious <166190036+DC125Konscious@users.noreply.github.com>
2024-05-17 11:38:10 -0400

    Title Menu, HUB, stash, and mission
    Godot nodes worked, nodes added, music added, some sounds added.

--- GD Scripts/main_menu.gd
+++ GD Scripts/main_menu.gd
@@ -5,6 +5,9 @@ extends CanvasLayer
 @onready var options_menu: Control = $Options_Menu
 @onready var load_screen: Control = $Load_Screen
 
+func sounds():
+	$MenuSound.play()
+
 func _ready() -> void:
 	animation_player.play("guns")
 	
@@ -14,8 +17,15 @@ func _on_quit_button_pressed() -> void:
 func _on_option_button_pressed() -> void:
 	options_menu.show()
 
-
 func _on_start_button_pressed() -> void:
 	load_screen.StartLoading()
 func _on_button_pressed() -> void:
 	options_menu.hide()
+
+#// Sounds for mouse hovering buttons 
+func _on_start_button_mouse_entered() -> void:
+	sounds()
+func _on_option_button_mouse_entered():
+	sounds()
+func _on_quit_button_mouse_entered():
+	sounds()
