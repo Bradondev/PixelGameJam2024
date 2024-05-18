0d61bbb55e9c2e0f74cc5f9d4ead050917042138
WatermelonFroggy <82559263+WatermelonFroggy@users.noreply.github.com>
2024-05-17 20:53:40 -0400

    Got AI schmooving
    Got AI schmooving, need help with wait functions

--- Components/Scripts/enemy_ai_component.gd
+++ Components/Scripts/enemy_ai_component.gd
@@ -5,67 +5,76 @@ class_name EnemyAIComponent
 @export var DetectionArea:AIDetectionArea
 @export var SMP:StateMachinePlayer
 @export var sprite:AnimatedSprite2D
+@export var timer:Timer
 
 var playingAnim: String = "Idle"
-var chasing:bool = false
 var bodyName: String
 var exitedBodyName: String
+var chasing:bool = false
 var player:Node
+var time:float = 0.0
 
 func _ready():
 	DetectionArea.body_entered.connect(_on_detection_area_body_entered)
 	DetectionArea.body_exited.connect(_on_detection_area_body_exited)
 	SMP.updated.connect(_on_state_machine_player_updated)
 
-func _process(delta):
-	if chasing:
-		MovableEntity.velocity = (player.position - MovableEntity.global_position).normalized() * MovableEntity.Stats.Speed * delta
-
 # Calls depending on the state machines update
 func _on_state_machine_player_updated(state, delta):
 	MovableEntity.velocity = Vector2.ZERO
+	print_debug(state)
 	match state:
 		"Idle":
 			chasing = false
 			if player:
-				SMP.set_trigger("PlayerDetected")
+				change_state("Chasing")
 			if not playingAnim == "Idle":
-				sprite.play_animation("Idle")
+				play_animation("Idle")
 		"Chasing":
 			if not player == null:
-				sprite.play_animation("Chasing")
+				play_animation("Chasing")
 				chasing = true
+				if MovableEntity.position.distance_to(player.position) <= 50:
+					change_state("Attacking")
 			else:
-				SMP.set_trigger("Idle")
+				change_state("Idle")
 		"Attacking":
+			time += delta
 			chasing = false
-			sprite.play_animation("Attacking")
-			wait(0.5)
-			SMP.set_trigger("Chasing")
+			play_animation("Attacking")
+			change_state("Chasing")
 		"Damaged":
+			time += delta
 			chasing = false
-			sprite.play_animation("Damaged")
-			wait(0.1)
-			SMP.set_trigger("Stunned")
+			play_animation("Damaged")
+			change_state("Chasing")
 		"Dead":
 			chasing = false
-			sprite.play_animation("Dead")
-			wait(0.5)
-			queue_free()
-
-func wait(seconds: float) -> void:
-	await(get_tree().create_timer(seconds).timeout)
+			play_animation("Dead")
+			change_state("Dead")
+			MovableEntity.kill_entity()
+	if chasing and player:
+		MovableEntity.velocity = (player.position - MovableEntity.global_position).normalized() * MovableEntity.Stats.Speed * delta
+	if MovableEntity.velocity.x < 0:
+		sprite.flip_h = true
+	if MovableEntity.velocity.x > 0:
+		sprite.flip_h = false
 
 func _on_detection_area_body_entered(body):
 	if "Player" in body.name:
-		print_debug("PlayerEntered")
 		player = body
 	exitedBodyName = ""
 	bodyName = body.name
 
 func _on_detection_area_body_exited(body):
 	if "Player" in body.name:
-		print_debug("PlayerExited")
 		player = null
 	bodyName = ""
 	exitedBodyName = body.name
+
+func play_animation(animation:String):
+	sprite.animation = animation
+	playingAnim = animation
+
+func change_state(state:String):
+	SMP.set_trigger(state)
